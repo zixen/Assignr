f35c3ffb9b95827718cf4204e447161686cd643e
Zixen Developer <dev@zixen.com>
2015-07-29 05:53:52 -0700

    Initial commit.

---
+++ Assignr/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Assignr.Startup))]
+namespace Assignr
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
